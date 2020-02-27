8f7a052525c16fd81e71b2a3b28c23f690f5c2cb
fujita-kait <hiro.fujita@he.kanagawa-it.ac.jp>
2020-02-27 16:21:26 +0900

    Initial Commit

---
+++ UDP-Cocoa/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UDP-Cocoa
+//
+//  Created by 藤田 on 2020/02/27.
+//  Copyright © 2020 KAIT. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
